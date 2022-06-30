760860674767406cb26afbd15cb4a223527af5fe
linjf <linjf@uia.no>
2022-06-30 15:45:32 +0200

    removed excess code

--- Sommerprosjekt_/Form2.Designer.cs
+++ Sommerprosjekt_/Form2.Designer.cs
@@ -35,20 +35,22 @@
             // header_lbl
             // 
             this.header_lbl.AutoSize = true;
-            this.header_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.header_lbl.Location = new System.Drawing.Point(29, 13);
+            this.header_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.header_lbl.Location = new System.Drawing.Point(29, 19);
             this.header_lbl.Name = "header_lbl";
-            this.header_lbl.Size = new System.Drawing.Size(0, 16);
+            this.header_lbl.Size = new System.Drawing.Size(60, 16);
             this.header_lbl.TabIndex = 2;
+            this.header_lbl.Text = "cxvcxvxc";
             // 
             // section_lbl
             // 
             this.section_lbl.AutoSize = true;
-            this.section_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.section_lbl.Location = new System.Drawing.Point(29, 80);
+            this.section_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.section_lbl.Location = new System.Drawing.Point(29, 50);
             this.section_lbl.Name = "section_lbl";
-            this.section_lbl.Size = new System.Drawing.Size(0, 16);
+            this.section_lbl.Size = new System.Drawing.Size(30, 15);
             this.section_lbl.TabIndex = 3;
+            this.section_lbl.Text = "xcvc";
             // 
             // Form2
             // 
--- Sommerprosjekt_/Program.cs
+++ Sommerprosjekt_/Program.cs
@@ -6,12 +6,6 @@ namespace Sommerprosjekt_
     internal static class Program
     {
         
-        public class Foo
-        {
-            //global variables to display the correct header and section from form 1 in form 2
-            public static string header = "";
-            public static string section = ""; 
-        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
